3d8eeba2b2201cbf079cd0f66977e130cab9d3ed
Kainoa Kanter <kainoa@t1c.dev>
2024-11-06 13:58:22 -0300

    bluesky, up deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    optimizeHoistedScript: true,
-  },
-
   integrations: [
     icon({
       include: {
